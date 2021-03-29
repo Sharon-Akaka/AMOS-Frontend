fb98fdb419d7f32925c501c4e28550af8b71fac5
Matthew Man <matthew08m@gmail.com>
2021-03-29 16:36:41 +0100

    Added input states and ability to handle input submissions

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -1,14 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './components.css';
 
 export default function InputForm() {
+    const [titleInput, setTitleInput] = useState("");
+    const [authorInput, setAuthorInput] = useState("");
+    const [urlInput, setUrlInput] = useState("");
+    const [descriptionInput, setDescriptionInput] = useState("");
+    
+    async function handleSubmit() {
+        try {
+            const body = {
+                title: titleInput,
+                author: authorInput,
+                url: urlInput,
+                description: descriptionInput
+            }
+            console.log(body)
+            await fetch("", {
+                method: "POST",
+                body: JSON.stringify(body),
+                headers: {
+                    "Content-Type": "application/json"
+                }
+            })
+            setTitleInput("");
+            setAuthorInput("");
+            setUrlInput("");
+            setDescriptionInput("");
+        } catch (error) {
+            console.error(error.message)
+        }
+    }
+
     return(
         <div className='input-container'>
-            <input type="text" placeholder='Title'/>
-            <input type="text" placeholder='Author'/>
-            <input type="text" placeholder='URL'/>
-            <textarea className='textarea-input' placeholder='Description'></textarea>
-            <button className='submit-btn'>Submit</button>
+            <input value={titleInput} onChange={e => setTitleInput(e.target.value)} type="text" placeholder='Title'/>
+            <input value={authorInput} onChange={e => setAuthorInput(e.target.value)} type="text" placeholder='Author'/>
+            <input value={urlInput} onChange={e => setUrlInput(e.target.value)} type="text" placeholder='URL'/>
+            <textarea value={descriptionInput} onChange={e => setDescriptionInput(e.target.value)} className='textarea-input' rows={5} placeholder='Description'></textarea>
+            <button onClick={() => handleSubmit()} className='submit-btn'>Submit</button>
         </div>
     )
 }
