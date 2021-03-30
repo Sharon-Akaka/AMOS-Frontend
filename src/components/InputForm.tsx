99502f6e56769eb47d62192dfa69733a5d8790cf
Sharon Akaka <sharon.akaka@gmail.com>
2021-03-30 14:27:57 +0100

    Added extra inputs

--- src/components/InputForm.tsx
+++ src/components/InputForm.tsx
@@ -6,15 +6,19 @@ export default function InputForm() {
     const [authorInput, setAuthorInput] = useState("");
     const [urlInput, setUrlInput] = useState("");
     const [descriptionInput, setDescriptionInput] = useState("");
-    const [categoryInput, setCategoryInput] = useState("")
+    const [categoryInput, setCategoryInput] = useState("");
+    const [recommendedInput, setRecommendedInput] = useState("");
+    const [isFaculty, setIsFaculty] = useState(false);
+    const [usedResource, setUsedResource] = useState(false);
 
-    
+    // console.log(recommendedInput, isFaculty, usedResource)
+
     function categoryToArray(input: string): string[] {
         const array = input.replace(/\s+/g, "");
         return array.split(",")
     }
 
-    
+
     async function handleSubmit() {
         try {
             const body = {
@@ -22,7 +26,10 @@ export default function InputForm() {
                 author: authorInput,
                 url: urlInput,
                 description: descriptionInput,
-                cat_tags: categoryToArray(categoryInput)
+                cat_tags: categoryToArray(categoryInput),
+                recommeded: recommendedInput,
+                is_faculty: isFaculty,
+                was_used:usedResource
             }
             console.log(body)
             await fetch("https://study-resources-app.herokuapp.com/", {
@@ -37,18 +44,35 @@ export default function InputForm() {
             setAuthorInput("");
             setUrlInput("");
             setDescriptionInput("");
+            setRecommendedInput("");
+            setIsFaculty(false);
+            setUsedResource(false);
+
+
+
         } catch (error) {
             console.error(error.message)
         }
     }
 
-    return(
+    return (
         <div className='input-container'>
-            <input value={titleInput} onChange={e => setTitleInput(e.target.value)} type="text" placeholder='Title'/>
-            <input value={authorInput} onChange={e => setAuthorInput(e.target.value)} type="text" placeholder='Author'/>
-            <input value={urlInput} onChange={e => setUrlInput(e.target.value)} type="text" placeholder='URL'/>
-            <textarea value={descriptionInput} onChange={e => setDescriptionInput(e.target.value)} className='textarea-input' rows={5} placeholder='Description'></textarea>
-            <input type="text" placeholder="Category - e.g. cat1, cat2, cat3..." value={categoryInput} onChange={e => setCategoryInput(e.target.value)}/>
+            <label>Title</label>
+            <input value={titleInput} onChange={e => setTitleInput(e.target.value)} type="text" placeholder='e.g. TypeScript tutorial' />
+            <label>Author</label>
+            <input value={authorInput} onChange={e => setAuthorInput(e.target.value)} type="text" placeholder='e.g Codecademy' />
+            <label>URL</label>
+            <input value={urlInput} onChange={e => setUrlInput(e.target.value)} type="text" placeholder='URL' />
+            <label>Description</label>
+            <textarea value={descriptionInput} onChange={e => setDescriptionInput(e.target.value)} className='textarea-input' rows={5} placeholder='Type here ...(max 400 characters)'></textarea>
+            <label>Categories</label>
+            <input type="text" placeholder="Category - e.g. cat1, cat2, cat3..." value={categoryInput} onChange={e => setCategoryInput(e.target.value)} />
+            <hr />
+            <label>Recommended by</label>
+            <input type='text' placeholder='Your name' value={recommendedInput} onChange={e => setRecommendedInput(e.target.value)} />
+            <label>Are you faculty? <input type='checkbox' checked={isFaculty} onChange={() => setIsFaculty(!isFaculty)}/></label>
+            <label>Have you used this resource?<input type='checkbox' checked={usedResource} onChange={() => setUsedResource(!usedResource)}/></label>
+
             <button onClick={() => handleSubmit()} className='submit-btn'>Submit</button>
         </div>
     )
