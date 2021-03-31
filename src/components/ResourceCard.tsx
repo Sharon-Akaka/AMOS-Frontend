620b9032ca6462e8685c4eab73336a591260ceb5
Sharon Akaka <sharon.akaka@gmail.com>
2021-03-31 10:56:44 +0100

    Added vote buttons

--- src/components/ResourceCard.tsx
+++ src/components/ResourceCard.tsx
@@ -1,3 +1,4 @@
+import { NONAME } from "node:dns";
 import React from "react";
 import './components.css';
 
@@ -16,19 +17,48 @@ export interface IData {
     recommender?: string,
     is_faculty: boolean,
     mark_stage?: string,
-    was_used: boolean
+    was_used: boolean,
+    vote: number
 }
 
 export default function ResourceCard(props: IResourceCard) {
+    // const [] = useState()
     const { data } = props
+
+    async function handleUpVote() {
+        await fetch(`https://study-resources-app.herokuapp.com/upvote/${data.id}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+    async function handleDownVote() {
+        await fetch(`https://study-resources-app.herokuapp.com/downvote/${data.id}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+    }
+    console.log(data.url);
     return (
         <div className='resource-card-container'>
-            <div className='card-title'>
-                <h2>{data.title}</h2>
-                <p className='author-txt'>{data.author}</p>
+            <div className='voting-container'>
+                <li><button onClick={handleUpVote}>⬆️</button></li>
+                <li>number</li>
+                <li><button onClick={handleDownVote}>⬇️</button></li>
             </div>
-            <p>{data.description}</p>
-            <p className='categories-txt'>Categories: {data.cat_tags}</p>
+            <div className='card-content'>
+                <div className='card-title'>
+                    {/* Find long term fix to the http issue */}
+                    <h2><a href={"https://" + data.url} target="_blank">{data.title}</a></h2>
+                    <p className='author-txt'>{data.author}</p>
+                </div>
+                <p>{data.description}</p>
+                <p className='categories-txt'>Categories: {data.cat_tags}</p>
+            </div>
+
         </div>
     )
 }
